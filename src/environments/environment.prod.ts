db29cf5d3c233ea1cb22c46452f29f41cf1d312e
Catalin Scurt <cata_scurt@yahoo.com>
2023-04-11 18:08:58 +0300

    change api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'https://api.openweathermap.org/data/2.5/weather?',
-  apiKey: '0c30b12211d6f45c4e33487ea592919f',
   baseUrl: 'http://api.weatherapi.com/v1/forecast.json?',
-  apiKEY: 'b8728d0881cd4fa6acc53116230504'
+  apiKEY: '733a24a7fa6a44f8a3a145533231104'
 };
